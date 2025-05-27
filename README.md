66ec01c218df68aed5ab79c9d9e57ed7f00ebdf3
rinsuki <428rinsuki+git@gmail.com>
2025-05-28 07:12:58 +0900

    fix some types and add readme

--- README.md
+++ README.md
@@ -2,6 +2,19 @@
 
 very WIP, but I wanted to automatically generate TypeScript typing files from MusicBrainz source code.
 
+## Example
+
+```typescript
+import { isReleaseRelationshipEditor } from "typedbrainz";
+
+function currentReleaseName(): string | null {
+    const relationshipEditor = window.MB?.relationshipEditor;
+    if (relationshipEditor == null) return null
+    if (!isReleaseRelationshipEditor(relationshipEditor)) return null
+    return relationshipEditor.state.entity.name;
+}
+```
+
 ## LICENSE
 
 * `converter/**`: see `SPDX-License-Identifier`
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 // SPDX-License-Identifier: MIT
 
 import type { MaybeReleaseRelationshipEditor, NonReleaseRelationshipEditor, RelationshipEditorBase, ReleaseRelationshipEditor } from "./global.js";
+import { $_$Spread } from "./type-utils.js";
 
 type EveryPropertyIsExists<T> = {
     [K in keyof T]-?: T[K] extends undefined | null ? never : T[K]
 }
 
-export function isReleaseRelationshipEditor(relationshipEditor: MaybeReleaseRelationshipEditor): relationshipEditor is RelationshipEditorBase & EveryPropertyIsExists<ReleaseRelationshipEditor> {
+export function isReleaseRelationshipEditor(relationshipEditor: MaybeReleaseRelationshipEditor): relationshipEditor is $_$Spread<EveryPropertyIsExists<ReleaseRelationshipEditor>, RelationshipEditorBase> {
     return relationshipEditor.state?.entity.entityType === "release";
 }
 
-export function isNonReleaseRelationshipEditor(relationshipEditor: MaybeReleaseRelationshipEditor): relationshipEditor is RelationshipEditorBase & EveryPropertyIsExists<NonReleaseRelationshipEditor> {
+export function isNonReleaseRelationshipEditor(relationshipEditor: MaybeReleaseRelationshipEditor): relationshipEditor is $_$Spread<EveryPropertyIsExists<NonReleaseRelationshipEditor>, RelationshipEditorBase> {
     return relationshipEditor.state?.entity.entityType !== "release";
 }
