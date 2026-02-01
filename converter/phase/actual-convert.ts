d0fead448b8a1f5dde95d5447054cf3a2c3e7874
rinsuki <428rinsuki+git@gmail.com>
2026-02-02 07:11:24 +0900

    chore: throw error with converting filename to easier debug

--- converter/phase/actual-convert.ts
+++ converter/phase/actual-convert.ts
@@ -20,39 +20,43 @@ export function doActualConvert(ctx: ConverterContext) {
     while (true) {
         const target = ctx.targets.pop()
         if (target == null) break
-        if (target.endsWith("src/type-utils.js")) continue
-        if (ctx.alreadyParsedTargets.has(target)) continue
-        ctx.alreadyParsedTargets.add(target)
-        if (!target.endsWith(".js") && !target.endsWith(".mjs")) {
-            console.warn("Skipping non-JS file:", target)
-            continue
-        }
-        const src = readFileSync(target, "utf-8")
-        const ast = parse(src)
-        ctx.currentFilePath = target
-        // console.log(ast)
-        // console.log("Converting", target)
-        let dest = createSourceFile("", compatibilityFile.replace(originalTypeUtilsPath, relative(
-            dirname(join(process.cwd(), target)),
-            originalTypeUtilsRealPath,
-        )), ScriptTarget.ESNext)
-        const statements = (ast.body as any[]).flatMap(ast => {
-            // console.log(src.slice(...ast.range))
-            return convertAST(ctx, ast)
-        })
-        dest = appended(dest, statements.filter(x => x != null))
-        for (const s of dest.statements) {
-            if (isImportDeclaration(s)) {
-                const module = s.moduleSpecifier
-                assert(isStringLiteral(module))
-                if (module.text.startsWith(".")) {
-                    ctx.targets.push(join(dirname(target), module.text))
+        try {
+            if (target.endsWith("src/type-utils.js")) continue
+            if (ctx.alreadyParsedTargets.has(target)) continue
+            ctx.alreadyParsedTargets.add(target)
+            if (!target.endsWith(".js") && !target.endsWith(".mjs")) {
+                console.warn("Skipping non-JS file:", target)
+                continue
+            }
+            const src = readFileSync(target, "utf-8")
+            const ast = parse(src)
+            ctx.currentFilePath = target
+            // console.log(ast)
+            // console.log("Converting", target)
+            let dest = createSourceFile("", compatibilityFile.replace(originalTypeUtilsPath, relative(
+                dirname(join(process.cwd(), target)),
+                originalTypeUtilsRealPath,
+            )), ScriptTarget.ESNext)
+            const statements = (ast.body as any[]).flatMap(ast => {
+                // console.log(src.slice(...ast.range))
+                return convertAST(ctx, ast)
+            })
+            dest = appended(dest, statements.filter(x => x != null))
+            for (const s of dest.statements) {
+                if (isImportDeclaration(s)) {
+                    const module = s.moduleSpecifier
+                    assert(isStringLiteral(module))
+                    if (module.text.startsWith(".")) {
+                        ctx.targets.push(join(dirname(target), module.text))
+                    }
                 }
             }
+            const printer = createPrinter()
+            const desttext = applyPatches(printer.printFile(dest))
+            mkdirSync(dirname(target.replace("upstream", "generated")), { recursive: true })
+            writeFileSync(target.replace("upstream", "generated").replace(/(\.m?)js$/, "$1ts"), desttext)
+        } catch (e) {
+            throw new Error(`Failed to convert ${target}`, { cause: e as Error } )
         }
-        const printer = createPrinter()
-        const desttext = applyPatches(printer.printFile(dest))
-        mkdirSync(dirname(target.replace("upstream", "generated")), { recursive: true })
-        writeFileSync(target.replace("upstream", "generated").replace(/(\.m?)js$/, "$1ts"), desttext)
     }
 }
