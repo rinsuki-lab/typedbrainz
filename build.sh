c76fabad18a85df229e8cb896b692a9ebc3e71e9
rinsuki <428rinsuki+git@gmail.com>
2026-04-10 19:23:18 +0900

    chore: update weight-balanced-tree

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -xe
 rm -rf generated lib
 tsimp ./converter/index.ts
 dts-bundle-generator ./src/global.d.ts -o ./lib/global.d.ts --no-check --inline-declare-global
-dts-bundle-generator ./node_modules/weight-balanced-tree/index.d.ts -o ./lib/weight-balanced-tree.d.ts --external-inlines weight-balanced-tree
+dts-bundle-generator ./node_modules/weight-balanced-tree/types/index.d.ts -o ./lib/weight-balanced-tree.d.ts --external-inlines weight-balanced-tree
 sed -i.bak 's/weight-balanced-tree/.\/weight-balanced-tree.js/' ./lib/global.d.ts
 rm ./lib/global.d.ts.bak
 echo -e "// THIS FILE IS GENERATED FROM THIRD-PARTY https://www.npmjs.com/package/weight-balanced-tree (to reduce dependencies)\n// SPDX-License-Identifier: MIT" | cat - ./lib/weight-balanced-tree.d.ts > ./lib/temp
